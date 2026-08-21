453aefafc6e1d9c18e294d249a9628e948313524
tasx13ok-create <tasx13ok@gmail.com>
2026-08-21 12:08:13 -0700

    Explicitly allow search and AI discovery crawlers

--- app/robots.ts
+++ app/robots.ts
@@ -1,16 +1,36 @@
 import type { MetadataRoute } from 'next'
 
 const SITE_URL = 'https://proofttl-web.vercel.app'
+const PRIVATE_PATHS = ['/api/', '/account/', '/console/']
 
 export const dynamic = 'force-static'
 
 export default function robots(): MetadataRoute.Robots {
+  const discoveryBots = [
+    'OAI-SearchBot',
+    'ChatGPT-User',
+    'GPTBot',
+    'PerplexityBot',
+    'ClaudeBot',
+    'Googlebot',
+    'Google-Extended',
+    'bingbot',
+    'CCBot',
+  ]
+
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/api/', '/account/', '/console/'],
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: PRIVATE_PATHS,
+      },
+      ...discoveryBots.map((userAgent) => ({
+        userAgent,
+        allow: '/',
+        disallow: PRIVATE_PATHS,
+      })),
+    ],
     sitemap: `${SITE_URL}/sitemap.xml`,
     host: SITE_URL,
   }
