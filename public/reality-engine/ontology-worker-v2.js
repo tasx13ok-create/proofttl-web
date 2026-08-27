11f0525a6ab32358148874f40d62530461b7cbe8
tasx13ok-create <tasx13ok@gmail.com>
2026-08-26 20:52:11 -0400

    Promote Reality Engine worker to v0.6 Depth Court

--- public/reality-engine/ontology-worker-v2.js
+++ public/reality-engine/ontology-worker-v2.js
@@ -1,6 +1,6 @@
 'use strict'
-importScripts('./ontology-engine-v2.js','./ontology-engine-v3.js','./ontology-engine-v4.js')
-const E=self.OntologyEngineV4
+importScripts('./ontology-engine-v2.js','./ontology-engine-v3.js','./ontology-engine-v4.js','./ontology-engine-v5.js')
+const E=self.OntologyEngineV5
 self.onmessage=e=>{
   const msg=e.data||{}
   try{
