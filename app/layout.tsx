9b862af5d40e03b1a80b32fd85c9b539adf6d070
tasx13ok-create <tasx13ok@gmail.com>
2026-09-01 10:37:39 -0400

    Load shared product header styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,7 @@ import './cinematics.css'
 import './premium-public.css'
 import './centered-ui.css'
 import './detail-pages.css'
+import './shared-product-header.css'
 
 const SITE_URL = 'https://proofttl-web.vercel.app'
 const GITHUB_PROFILE = 'https://github.com/tasx13ok-create'
